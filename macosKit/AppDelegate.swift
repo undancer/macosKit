f27cc1a2d9f3db32145028bad8e40ff0fe62aaea
undancer <undancer@gmail.com>
2018-10-25 15:27:52 +0800

    first commit

---
+++ macosKit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macosKit
+//
+//  Created by undancer on 2018/10/25.
+//  Copyright © 2018 undancer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
